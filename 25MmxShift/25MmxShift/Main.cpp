0639ba2bfca54f6c106c117d8b859d20493d6cb5
Benny Franco <bennyfranco@icloud.com>
2019-08-04 21:44:58 -0700

    Mmx Shift init

---
+++ 25MmxShift/25MmxShift/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+enum MmxShiftOp : unsigned int
+{
+	psllw,			// shift left logical word
+	psrlw,			// shift right logical word
+	psraw,			// shift right arithmetic word
+	pslld,			// shift left logical dword
+	psrld,			// shift right logical dword
+	psrad,			// shift right arithmetic dword
+};
+
+int main()
+{
+	return 0;
+}
